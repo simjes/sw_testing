d9d2f1abface4867a59bf2721a696ad2bfb1a13a
skilbert <jostein1993@hotmail.com>
2016-03-04 15:34:00 +0100

    commented

--- Minesweeper-Java/MineField.java
+++ Minesweeper-Java/MineField.java
@@ -5,6 +5,9 @@ class MineField{
 	private boolean boom;
 	private final int rowMax = 5;
 	private final int colMax = 10;
+	/**
+	 * constructor. Generates a minefield with random values
+	 */
 	MineField(){
 		mines=new boolean[rowMax][colMax];
 		visible=new boolean[rowMax][colMax];
@@ -26,6 +29,12 @@ class MineField{
 			}
 		}
 	}	
+	/**
+	 * check if it the move is valid
+	 * @param randomRow random row
+	 * @param randomCol random col
+	 * @return returns false if it is. Sets the move too true and returns true if it not taken
+	 */
 	private boolean trymove(int randomRow, int randomCol) {
 		if(mines[randomRow][randomCol]){
 			return false;
@@ -35,6 +44,9 @@ class MineField{
 			return true;
 		}
 	}
+	/**
+	 * iterates the field and sets visible 2d array to true if there is a mine on current position. sets boom to true when done and calls show()
+	 */
 	private void boom() {
 		for(int row=0;row<rowMax;row++){
 			for(int col=0;col<colMax;col++){
@@ -46,6 +58,12 @@ class MineField{
 		boom=true;
 		show();
 	}
+	/**
+	 * show the current content of position given by row col
+	 * @param row
+	 * @param col
+	 * @return returns what should be located under the postion in form of a char
+	 */
 	private char drawChar(int row, int col) {
 		int count=0;
 		if(visible[row][col]){
@@ -64,12 +82,22 @@ class MineField{
 			}
 			return '?';
 		}
+		//edited from original solution. This one saves a couple of lines
 		if(count < 9) return (char) (count+48);
 		else return 'X';
 	}
+	/**
+	 * returns current boom status
+	 * @return
+	 */
 	public boolean getBoom(){
 		return boom;
 	}
+	/**
+	 * parses the string input and decide if it is a legal move. calles legalmoveValue if it is
+	 * @param input
+	 * @return true if it is a legal move. False else
+	 */
 	public boolean legalMoveString(String input) {
 		String[] separated=input.split(" ");
 		int row;
@@ -93,6 +121,12 @@ class MineField{
 			return false;
 		}
 	}
+	/**
+	 * handles the values given by the user. Sets the position to visible and checks for mines
+	 * @param row
+	 * @param col
+	 * @return returns true if it is legal move. False if it is not
+	 */
 	private boolean legalMoveValue(int row, int col) {	
 		if(visible[row][col]){
 			System.out.println("You stepped in allready revealed area!");
@@ -107,6 +141,9 @@ class MineField{
 		}
 		return true;
 	}
+	/**
+	 * prints the minefield
+	 */
 	public void show() {
 		System.out.println("\n    0 1 2 3 4 5 6 7 8 9 ");
 		System.out.println("   ---------------------");
