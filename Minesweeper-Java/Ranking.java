70f0bcbc9d5820db379fdf0a4ce2b15fcd38b12c
skilbert <jostein1993@hotmail.com>
2016-03-04 14:57:39 +0100

    made more compact

--- Minesweeper-Java/Ranking.java
+++ Minesweeper-Java/Ranking.java
@@ -1,46 +1,34 @@
 import java.util.Scanner;
 
 public class Ranking{
-
 	private final int MAX_PEOPLE_LIMIT=5;
 	private String[] name;
 	private int[] record;
 	private int last;
-	
 	Ranking(){
 		name=new String[MAX_PEOPLE_LIMIT];
 		record=new int[MAX_PEOPLE_LIMIT];
-		
 		last=0;
 	}
-
-
 	public void recordName(int result) {
 		System.out.print("\n Please enter your name -");
 		Scanner in=new Scanner(System.in);
 		String newName=in.nextLine();
 		if((last==MAX_PEOPLE_LIMIT)&&record[MAX_PEOPLE_LIMIT-1]>result){
 			System.out.println("\nSorry you cannot enter top "+(MAX_PEOPLE_LIMIT)+" players");
-			return;
 		}
 		else if(last==MAX_PEOPLE_LIMIT){
-			name[MAX_PEOPLE_LIMIT-1]=newName;{
-				record[MAX_PEOPLE_LIMIT-1]=result;
-			}
+			name[MAX_PEOPLE_LIMIT-1]=newName;
+			record[MAX_PEOPLE_LIMIT-1]=result;
 		}
 		else{
-			name[last]=newName;{
-				record[last]=result;{
-					last++;
-				}
-			}
+			name[last]=newName;
+			record[last]=result;
+			last++;
 		}
-		
 		sort();
 		show();
 	}
-
-
 	public void show() {
 		if(last==0){
 			System.out.println("Still no results");
@@ -60,15 +48,13 @@ public class Ranking{
 			unsorted=false;
 			for(int i=0;i<(last-1);i++){
 				if(record[i+1]>record[i]){
-					int swapR=record[i];{
-						record[i]=record[i+1];{
-							record[i+1]=swapR;
-							String swapN=name[i];
-							name[i]=name[i+1];
-							name[i+1]=swapN;
-							unsorted=true;
-						}
-					}
+					int swapR=record[i];
+					record[i]=record[i+1];
+					record[i+1]=swapR;
+					String swapN=name[i];
+					name[i]=name[i+1];
+					name[i+1]=swapN;
+					unsorted=true;
 				}
 			}
 		}
